86c2abc8a002eb816be9b6ed8cc3d1009ea334ab
Faith Wairimu Gachemi <faithnimoh7@gmail.com>
2025-03-18 15:02:58 +0300

    Lists Assignment

---
+++ listassign.py
@@ -0,0 +1,16 @@
+# Creating an empty list
+my_list = []
+#appending elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#extending elements
+my_list.extend([50,60,70,])
+#removing  elements
+del my_list[40]
+#sorting elements 
+my_list.sort()
+#find index of an element
+print(my_list.index(30))
+print(my_list)
