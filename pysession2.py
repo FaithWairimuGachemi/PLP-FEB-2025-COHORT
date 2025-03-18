e2420b6b02fc89842f9426e4e79aa8de0fe893b6
Faith Wairimu Gachemi <faithnimoh7@gmail.com>
2025-03-18 11:35:55 +0300

    Add files via upload

---
+++ pysession2.py
@@ -0,0 +1,25 @@
+#lists-enclosed using normal brackets
+students = ("Faith" , "Mary" , "Jane" , "Joseph")
+print(students)
+print(students[1])
+#dictionary-enclosed using curly brackets
+Countries ={
+    "Kenya" : "Nairobi"
+}
+student ={
+    "name" : "Faith",
+    "age" : "20",
+    "gender" : "Female",
+    "My_name" : {
+        "first_name" : "Faith" ,
+        "last_name" : "Wairimu"
+        }
+}
+
+student["height"] = 170
+print(student)
+print(student["age"])
+ 
+#sets
+students_ids ={ 1, 2, 3, 4, "Jack" , True}
+print(students_ids)
