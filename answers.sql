ac974823ceed25d4955fa3baf0cdc46d73c05e36
Faith Wairimu Gachemi <faithnimoh7@gmail.com>
2025-05-14 15:42:57 +0300

    Week 2 database assignment

--- answers.sql
+++ answers.sql
@@ -1,3 +1,25 @@
-CREATE DATABASE salesDB;
+--query to retrieve
+SELECT checkNumber, paymentDate, amount
+FROM payments;
 
-DROP DATABASE IF EXISTS demo;
+--query to retrive and sort
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+
+--query to display selected and sort
+SELECT firstName, lastName, email
+FROM employees
+WHERE jobTitle = 'Sales Rep'
+ORDER BY employeeNumber DESC;
+
+--query to retrieve all
+SELECT *
+FROM offices;
+
+--query to fetch with limits
+SELECT productName, quantityInStock
+FROM products
+ORDER BY buyPrice ASC
+LIMIT 5;
